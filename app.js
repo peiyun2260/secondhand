dfc2f041eb10bf75cfcb8f5e64473e80f4e3bf03
peiyun2260 <56057139+peiyun2260@users.noreply.github.com>
2025-01-08 21:50:26 +0800

    commented out for testing

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ app.use(
 );
 
 // 明確處理預檢請求
-app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(204);
-});
+// app.options("*", (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, OPTIONS"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.sendStatus(204);
+// });
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
