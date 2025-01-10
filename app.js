806f9fdb5cadf138ec6bdabae46fc7c4021a79b2
peiyun2260 <56057139+peiyun2260@users.noreply.github.com>
2025-01-10 15:54:07 +0800

    revert file

--- app.js
+++ app.js
@@ -20,23 +20,12 @@ app.use(
 );
 
 // 明確處理預檢請求
-// app.options("*", (req, res) => {
-//   res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, OPTIONS, PATCH"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.sendStatus(204);
-// });
-
 app.options("*", (req, res) => {
-  const allowedOrigin = req.headers.origin;
-  if (["https://secondhandhand22.wixsite.com", "https://secondhandhand22.wixsite.com/my-site-1", "http://localhost:3000"].includes(allowedOrigin)) {
-    res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
-  }
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS, PATCH"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.sendStatus(204);
