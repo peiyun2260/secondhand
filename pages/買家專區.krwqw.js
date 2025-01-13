d1d21f75fb0d0f6520999ccbbfcdac619579683d
peiyun2260 <56057139+peiyun2260@users.noreply.github.com>
2025-01-13 22:11:29 +0800

    Delete pages directory

--- pages/買家專區.krwqw.js
+++
@@ -1,9 +0,0 @@
-// Velo API 參照： https://www.wix.com/velo/reference/api-overview/introduction
-
-import wixLocation from 'wix-location';
-
-$w.onReady(function () {
-    $w('#button2').onClick(() => {
-        wixLocation.to("/shang-pin-ping-lun/"); 
-    });
-});
